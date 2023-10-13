e2355d85c529a7ed9681bec1eefd1a7dc21a8c3b
bom <bibidibovidiboo@gmail.com>
2023-10-13 16:10:00 +0900

    Add file about alias type example

---
+++ 1-TYPES/1-4-alias.ts
@@ -0,0 +1,10 @@
+{
+  // 타입스크립트의 꽃 🌷
+  // 원하는 타입을 지정할 수있음
+  type Text = string;
+  const name: Text = 'bomi';
+
+  // String literal types
+  type Name = 'bomi';
+  const boming: Name = 'bomi';
+}
