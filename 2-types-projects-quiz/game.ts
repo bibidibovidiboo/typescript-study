4ff517699db554ec5d3f1caeff29530e80ad1507
bibidibovidiboo <bibidibovidiboo@gmail.com>
2023-10-16 13:21:01 +0900

    Add files about types-project-quiz example

---
+++ 2-types-projects-quiz/game.ts
@@ -0,0 +1,35 @@
+/**
+ * Let's make a game 좌표게임 🕹
+ */
+
+const position = { x: 0, y: 0 };
+type direction = 'up' | 'down' | 'left' | 'right';
+function move(direction: direction) {
+  switch (direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y -= 1;
+      break;
+    case 'left':
+      position.x -= 1;
+      break;
+    case 'right':
+      position.x += 1;
+      break;
+    default:
+      throw new Error(`unknown direction: ${direction}`);
+  }
+}
+
+// test log
+console.log(position); // { x: 0, y: 0}
+move('up');
+console.log(position); // { x: 0, y: 1}
+move('down');
+console.log(position); // { x: 0, y: 0}
+move('left');
+console.log(position); // { x: -1, y: 0}
+move('right');
+console.log(position); // { x: 0, y: 0}
